f5b713db85dbf53763dde6ff1c3c070242980e88
v.rumyantsev <rumyantsev1990@gmail.com>
2020-02-07 07:57:24 +0300

    add example Kotlin

--- kotlin-examples/src/main/kotlin/example/NpeExample.kt
+++ kotlin-examples/src/main/kotlin/example/NpeExample.kt
@@ -18,3 +18,38 @@ private fun example1() {
 }
 
 private fun getNullResult(): String? = null
+
+fun example(){
+    val str : String? = null
+    var length = str?.length // null
+}
+
+class Inner{
+    var first : String? = null
+    var second : Inner2 = Inner2()
+
+    class Inner2{
+        var firs : String? = null
+    }
+}
+
+fun example2(){
+    val inner : Inner? = null
+    print(inner?.second?.firs)
+}
+
+fun example3(){
+    val b : String? = if (Math.random() * 10 > 5) "test" else null
+    val l: Int = if (b != null) b.length else -1 // --> b?.length ?: -1
+    var lengthNotNull = b?.length ?: -1
+}
+
+fun example4(){
+    val b : String? = if (Math.random() * 10 > 5) "test" else null
+    val l = b!!.length // --> NullPointerException
+}
+
+fun example5(){
+    val nullableList: List<Int?> = listOf(1, 2, null, 4)
+    val intList: List<Int> = nullableList.filterNotNull()
+}
--- kotlin-examples/src/main/kotlin/example/PatMatExample.kt
+++ kotlin-examples/src/main/kotlin/example/PatMatExample.kt
@@ -22,3 +22,38 @@ class ValueWrapper(var value: Any) {
         println("test $value")
     }
 }
+
+fun example1() {
+    val a = if (Math.random() * 10 > 5) 5 else null
+    val aIntNotNull: Int? = a as Int
+    val aInt: Int? = a as? Int
+}
+
+fun example2() {
+    val obj: Any? = if (Math.random() * 10 > 5) 5 else "String"
+
+    if (obj is String) print(obj.length)
+
+    if (obj !is String) print("Not a String")
+    else print(obj.length)
+}
+
+fun example3(){
+    val x : Any? = null
+    when (x) {
+        is Int -> print(x + 1)
+        is String -> print(x.length + 1)
+        is IntArray -> print(x.sum())
+    }
+}
+
+fun example4(any : Any){
+    val nullSafe = any as? String // null
+    println(nullSafe)
+    val res = any as String // exception
+    println(res)
+}
+
+lateinit var date :Date
+lateinit var sqlDate : java.sql.Date // как в java
+lateinit var baseCalendar:  BaseCal // через as в kotlin
