6beec2fe182d75944e307255f2c72300339af1ab
n.shipyakov <nikolayshipyakov@gmail.com>
2020-02-11 00:50:39 +0300

    java-ex: two examples for pat mat

--- java-examples/src/main/java/examples/PatMatExample1.java
+++ java-examples/src/main/java/examples/PatMatExample1.java
@@ -3,12 +3,12 @@ package examples;
 public class PatMatExample1 {
 
     public static void main(String[] args) {
-        example8();
+        example9();
     }
 
     private static void example1() {
         Object obj = new SomeWrapper("pm");
-        if(obj instanceof SomeWrapper sw){
+        if (obj instanceof SomeWrapper sw) {
             /*
                Причем sw введено не просто так. Ибо нет уверенности, что obj не будет изменен после проверки
                например из другого потока. В Kotlin же только тоже не всегда работает smart cast
@@ -33,7 +33,7 @@ public class PatMatExample1 {
 
     private static void example3() {
         Object obj = "value";
-        if(obj instanceof String sw && sw.length() == 5) {
+        if (obj instanceof String sw && sw.length() == 5) {
             System.out.println("Работает");
         }
     }
@@ -57,7 +57,7 @@ public class PatMatExample1 {
 
     private static void example6() {
         Object obj = "value";
-        if(obj instanceof String sw && true) {
+        if (obj instanceof String sw && true) {
             System.out.println(sw.length());
         }
     }
@@ -65,16 +65,16 @@ public class PatMatExample1 {
     private static void example7() {
         Object obj = "value";
         // Интересный кейс
-        if(!(obj instanceof String str)) {
+        if (!(obj instanceof String str)) {
             throw new RuntimeException("incorrect type");
         }
         System.out.println(str.length());
     }
 
-    private static void example8(){
+    private static void example8() {
         Object obj = "value";
         // Интересно но довольно логично
-        if(obj instanceof String sw) {
+        if (obj instanceof String sw) {
             obj = "vv";
             System.out.println(sw.toUpperCase());
             System.out.println(obj);
@@ -83,6 +83,27 @@ public class PatMatExample1 {
 //        VALUE
 //        vv
 //        vv
+    }
+
+    private static void example9() {
+        // Еще одна причина почему используется доп переменная
+        SomeWrapper sw = new OtherWrapper("other");
+        if(sw instanceof OtherWrapper) {
+            sw.showText();
+            // Выводится Some wrapper. Если бы сделали приведение к OtherWrapper
+            // то вдруг стал выводится Other wrapper
+            // Это ломает обратную совметсиомть
+        }
+        if(sw instanceof OtherWrapper ow) {
+            ow.showText();
+            // Тут выведется Other wrapper
+        }
+    }
+
+    private void example10() {
+        if (getValue() instanceof String) {
+            // Не можем сослаться на значение которое вернул getValue
+        }
     }
 
     static class SomeWrapper {
@@ -100,5 +121,24 @@ public class PatMatExample1 {
         public void test() {
             System.out.println("test " + value);
         }
+
+        public static void showText() {
+            System.out.println("Some wrapper ");
+        }
+    }
+
+    static class OtherWrapper extends SomeWrapper {
+
+        public OtherWrapper(String value) {
+            super(value);
+        }
+
+        public static void showText() {
+            System.out.println("Other wrapper");
+        }
+    }
+
+    Object getValue(){
+        return "some string";
     }
 }
